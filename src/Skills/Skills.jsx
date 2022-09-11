1301499a943ba2dba88e951e614d782b459c69a6
claunicole <claunicole.gf@gmail.com>
2022-09-10 22:36:22 -0400

    Cambios en la paleta de colores

--- src/Skills/Skills.jsx
+++ src/Skills/Skills.jsx
@@ -37,6 +37,10 @@ function Skills() {
               <p>Git</p>
                 <img src="https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667" alt="git" className='skill-item' ></img>
         </div>
+        <div className='skills'>
+              <p>PostgreSQL</p>
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg" alt="Postgresql" className='skill-item' ></img>
+        </div>
         </div>
     </div>
   )

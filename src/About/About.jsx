4b474fdd360457e5e8845a0b1a078d337df3f141
claunicole <claunicole.gf@gmail.com>
2022-09-14 17:27:28 -0400

    Mejoras en la redireccion en rutas

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -8,6 +8,7 @@ function About() {
         <div className='about-text-container'>
         <p>Desarrolladora Web con conocimiento en HTML, CSS, JavaScript, React y Ruby on Rails.</p>
         <p>Apasionada por la tecnología, por el continuo aprendizaje, en búsqueda de retos y nuevas oportunidades.</p>
+        <p>Me encanta enseñar, tengo paciencia, mucha empatía para tratar con problemas, responsable y me adapto fácilmente a nuevas situaciones.</p>
         </div>
       </div>
     </div>

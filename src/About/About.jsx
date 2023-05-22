f7f61382403662b41345959dd79f74ef6322d300
claunicole <claunicole.gf@gmail.com>
2023-05-22 01:20:10 -0400

    style

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -11,7 +11,7 @@ function About() {
             <p>Desarrolladora Web con conocimiento en JavaScript, React, Next.js y Ruby on Rails.</p>
             <p>Apasionada por la tecnología, por el continuo aprendizaje, en búsqueda de retos y nuevas oportunidades.</p>
             <p>Actualmente trabajando en Código Facilito como Desarrolladora Ruby on Rails.</p>
-            <p>Tambien Mentora en BrightCoders, apoyando a participantes de los programas intensivos de Brightcoders a desarrollar aplicaciones buenas prácticas, mejorando sus habilidades técnicas y utilizando de forma continua tecnologías como Git y Github</p>
+            <p>También mentora en BrightCoders, apoyando a participantes de los programas intensivos de Brightcoders a desarrollar aplicaciones buenas prácticas, mejorando sus habilidades técnicas y utilizando de forma continua tecnologías como Git y Github</p>
         </div>
         </div>
       </div>

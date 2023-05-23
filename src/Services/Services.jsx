518cdbef0e4c1e7d7f589e0fdcc226cd3da64480
claunicole <claunicole.gf@gmail.com>
2023-05-23 15:13:34 -0400

    add lazy load image

--- src/Services/Services.jsx
+++ src/Services/Services.jsx
@@ -1,5 +1,10 @@
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 import { Link } from 'react-router-dom'
 import './Services.css'
+import landingImg from '/landing.png'
+import ecommerceImg from '/ecommerce.png'
+import codingImg from '/coding.png'
 
 function Services() {
   return (
@@ -8,7 +13,7 @@ function Services() {
         < div className='services'>
           <div className='service-card'>
             <div className='service-info'>
-            <img className='img-service' src='/landing.png' alt="a"/>
+            <LazyLoadImage className='img-service' alt='landing image' src={landingImg} effect='blur' />
               <h3 className='service-title'>Landing Page</h3>
               <h4 className='service-title'>100 usd</h4>
               <p>Sitio informativo, incluye inicio, sobre nosotros, servicios e implementacion de contacto por mail y/o whatsapp.</p>
@@ -20,7 +25,7 @@ function Services() {
           </div>
           <div className='service-card'>
             <div className='service-info'>
-            <img className='img-service' src='/ecommerce.png' alt="a"/>
+            <LazyLoadImage className='img-service' alt='ecommerce image' src={ecommerceImg} effect='blur' />
               <h3 className='service-title'>Ecommerce con Jumpseller</h3>
               <h4 className='service-title'>150usd</h4>
               <p>Diseño y maquetación, agregar productos, implementación de metodos de pago, metodos de envio, redes sociales y capacitacion para autoadministrar el sitio.</p>
@@ -31,7 +36,7 @@ function Services() {
           </div>
           <div className='service-card'>
             <div className='service-info'>
-            <img className='img-service' src='/coding.png' alt="a"/>  
+            <LazyLoadImage className='img-service' alt='coding image' src={codingImg} effect='blur' />
               <h3 className='service-title'>Aplicaciones Web</h3>
               <h4 className='service-title'>Valores según cotización</h4>
               <p>Aplicación desarrollada a medida.</p>
